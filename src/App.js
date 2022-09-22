a0ce9d6597e27cfd0b2370355ac98101243e70e9
rrom41k <roma13012003@mail.ru>
2022-09-22 19:20:08 +0300

    Добавление изображений из исходника

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div className="wrapper">
+      <header>
+        <div className = "headerLeft">
+          <svg />
+          <div className = "headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кросовок</p>
+          </div>
+        </div>
+        <ul className = "headerRight">
+          <li>
+            <svg />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>All Sneakers</h1>
+        ...
+      </div>
     </div>
   );
 }
